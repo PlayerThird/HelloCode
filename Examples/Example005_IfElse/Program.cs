2b3ec416de0e226cf7b43cef9a03f1bdee507e6f
PlayerThird030 <verevka.rud@gmail.com>
2022-12-14 00:15:08 +0300

    Example 5

---
+++ Examples/Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //ToLower() переводит текст в нижний регистр т.е. пофиг как писать маша-МаШа-и тд.
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
