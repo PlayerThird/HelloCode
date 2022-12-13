0f2f51a722665b2cd76a64e2637a3c25a6423657
PlayerThird030 <verevka.rud@gmail.com>
2022-12-14 00:06:15 +0300

    Example 3 добавили рандом

--- Examples/Example003_Math/Program.cs
+++ Examples/Example003_Math/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 3;
-int numberB = 10;
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿double numberA = new Random().Next(0, 11);// 0 1 2 ... 10
+Console.Write(numberA);
+Console.Write(" + ");
+double numberB = new Random().Next(0, 11);
+Console.Write(numberB);
+Console.Write(" = ");
+double result = numberA + numberB;
+Console.Write(result);
