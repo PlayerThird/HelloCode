845c51fccf7c0e1916d7d231db87faccf34effa4
PlayerThird030 <verevka.rud@gmail.com>
2022-12-14 21:39:03 +0300

    Задачка с собакой

---
+++ Examples/Dog/Program.cs
@@ -0,0 +1,21 @@
+﻿int count = 0;
+double distance = 100000;
+int firstFriendSpd = 1, secondFriendSpd = 2, dogSpd = 5;
+int friend = 2;
+double time = 0;
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance/(firstFriendSpd + dogSpd);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(secondFriendSpd + dogSpd);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpd + secondFriendSpd) * time;
+    count ++;
+}
+Console.WriteLine(count);
